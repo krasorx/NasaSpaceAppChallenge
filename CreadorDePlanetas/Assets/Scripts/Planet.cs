eb91789d93497e9e57967fc31b4c27295e9a9267
Luis <40400309+krasorx@users.noreply.github.com>
2019-10-26 23:07:25 -0300

    Add files via upload

---
+++ CreadorDePlanetas/Assets/Scripts/Planet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : Star
+{
+    
+    private float velocidadOrb;
+    public float distSol;
+    private float exentricidad;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exentricidad = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
